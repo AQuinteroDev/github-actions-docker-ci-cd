3c78968a7eb5c1c82378820f82bfbe1625f6f33a
aqp0001 <238456805+AQuinteroDev@users.noreply.github.com>
2026-02-09 20:13:31 +0100

    apuntes

--- src/app.js
+++
@@ -1,2 +0,0 @@
-// src/app.js
-console.log("¡Hola Alejandro! Ejecutando desde un contenedor vía GitHub Actions.");
--- src/app_test.js
+++
@@ -1,6 +0,0 @@
-Deno.test("Prueba básica de funcionamiento", () => {
-  const x = 1 + 1;
-  if (x !== 2) {
-    throw Error("¡Algo va muy mal!");
-  }
-});
